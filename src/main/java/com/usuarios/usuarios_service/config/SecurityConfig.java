8b89788f70e343dd60c00d3cbf2b8e3c66d94d96
Vivik <an.verde@duocuc.cl>
2026-05-14 20:09:02 -0400

    Fix

--- src/main/java/com/usuarios/usuarios_service/config/SecurityConfig.java
+++ src/main/java/com/usuarios/usuarios_service/config/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
 							})
 						)
 				// Se habilita el OAuth2 Login y despues redirecciona
-				.oauth2Login(oauth2 -> oauth2
+				.oauth2Login(oauth2 -> oauth2)
 						// .defaultSuccessUrl(
 						// 		frontUrl,
 						// 		true))
